853da5690a624aa0b8f51197f5c2df0b32750111
Nazi <mehdizadeh.sn@gmail.com>
2025-11-18 11:34:41 +0330

    necessary libraries imported

--- absolute/contracts/Presale.sol
+++ absolute/contracts/Presale.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Counter {
   uint public x;
