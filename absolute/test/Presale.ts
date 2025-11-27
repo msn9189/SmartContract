e1f353e24706ace333dc0cb641dfdfd9733e79a9
Nazi <mehdizadeh.sn@gmail.com>
2025-11-27 07:38:39 +0000

    a few tweaks fixed

--- absolute/test/Presale.ts
+++ absolute/test/Presale.ts
@@ -7,11 +7,22 @@ describe("TokenPresale", function () {
   let owner: any;
   let user1: any;
   let user2: any;
+  const startTime = BigInt("1764227834");
+  const endTime = BigInt("1764400634");
 
   beforeEach(async function () {
     [owner, user1, user2] = await ethers.getSigners();
     const token = await ethers.deployContract("MockToken", ["MockToken","MTK"]);
     await token.waitForDeployment();
-    const TokenPresale = await ethers.deployContract("PresaleToken");
+    const tokenAddress = await token.getAddress();
+    const TokenPresale = await ethers.deployContract("TokenPresale", [
+      tokenAddress,
+      ethers.parseUnits("2000", 18),
+      startTime,
+      endTime,
+      ethers.parseEther("0.1"),
+      ethers.parseEther("10"),
+      0
+    ]);
   });
 });  
