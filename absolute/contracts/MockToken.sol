e8dd7c3a99466dbb7c0fedde452988a822d48a3d
Nazi <mehdizadeh.sn@gmail.com>
2025-11-27 07:05:55 +0000

    mocktoken created

--- absolute/contracts/MockToken.sol
+++ absolute/contracts/MockToken.sol
@@ -3,3 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract MockToken is ERC20 {
+    constructor() ERC20("MOCKTOKEN", "MTK") {}
+
+    function mint(address to, uint256 value) external {
+        _mint(to, value);
+    }
+}
