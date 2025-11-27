08384b425558ec45727315651fe37009ebcbf54c
Nazi <mehdizadeh.sn@gmail.com>
2025-11-27 07:08:51 +0000

    beforeEach edited

--- absolute/test/Presale.ts
+++ absolute/test/Presale.ts
@@ -4,13 +4,13 @@ import { network } from "hardhat";
 const { ethers } = await network.connect();
 
 describe("TokenPresale", function () {
-    let owner: any;
-    let user1: any;
-    let user2: any;
+  let owner: any;
+  let user1: any;
+  let user2: any;
 
-    beforeEach(async function () {
-        const TokenPresale = await ethers.deployContract("PresaleToken");
-        [owner, user1, user2] = await ethers.getSigners();
-
-    })
-})
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+    const token = await ethers.deployContract("MockToken");
+    const TokenPresale = await ethers.deployContract("PresaleToken");
+  });
+});
