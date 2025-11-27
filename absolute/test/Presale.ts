78842fd44aa5fda792880d32f28e0e0538d5bfd3
Nazi <mehdizadeh.sn@gmail.com>
2025-11-27 06:36:05 +0000

    beforeEach defined in the test file

--- absolute/test/Presale.ts
+++ absolute/test/Presale.ts
@@ -1,5 +1,16 @@
 import { expect } from "chai";
 import { network } from "hardhat";
-import { it } from "mocha";
 
 const { ethers } = await network.connect();
+
+describe("TokenPresale", function () {
+    let owner: any;
+    let user1: any;
+    let user2: any;
+
+    beforeEach(async function () {
+        const TokenPresale = await ethers.deployContract("PresaleToken");
+        [owner, user1, user2] = await ethers.getSigners();
+
+    })
+})
