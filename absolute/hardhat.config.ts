99e643d5ab699e987a042702e2ec54159e734abf
Nazi <mehdizadeh.sn@gmail.com>
2025-12-03 19:57:10 +0000

    hardhatVerify imported from @nomicfoundation

--- absolute/hardhat.config.ts
+++ absolute/hardhat.config.ts
@@ -1,5 +1,6 @@
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable, defineConfig } from "hardhat/config";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 
 export default defineConfig({
   plugins: [hardhatToolboxMochaEthersPlugin],
